6756e6396c0d457a170012cbdb628d6d7db71b19
전은형 <ooooo5h91@gmail.com>
2021-04-18 22:46:46 +0900

    JSONArray -> JSONObject 추출 -> Project 형태로 변환 (Parsing)

--- app/src/main/java/com/neppplus/daily10minutes_difficult_20210418/MainActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_difficult_20210418/MainActivity.kt
@@ -3,6 +3,8 @@ package com.neppplus.daily10minutes_difficult_20210418
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.daily10minutes_difficult_20210418.datas.Project
+import com.neppplus.daily10minutes_difficult_20210418.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -31,6 +33,29 @@ class MainActivity : BaseActivity() {
 
 //        실제로 서버에서 받아오는 기능은 따로 만들자 코드정리겸
 
-    }
+        ServerUtil.getRequestProjectList(mContext, object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(jsonObj: JSONObject) {
 
+                val dataObj = jsonObj.getJSONObject("data")
+                val projectsArr = dataObj.getJSONArray("projects")
+
+//                반복문 for 문으로 projectsArr 내부를 하나씩 꺼내서 파싱하자
+
+                for (i in 0 until projectsArr.length()) {
+
+//                     [ ] 안에 있는 { } 들을, 하나씩 꺼낸 뒤,
+                    val projectObj = projectsArr.getJSONObject(i)
+
+//                     파싱 후 mProjects 에 추가하려고 하는데
+//                     Project() 에 오류가 생김.
+//                     Project 클래스에 보조 생성자 추가해서 재료없이도 만들 수 있게 하자
+
+//                     Project 클래스에 함수(기능) 추가 => JSONObject를 넣으면 Project형태로 변환해주는 기능
+//                     다른 화면에서도 쓸 기능이니까 Project에 함수로 만드는 거임.
+                    val project = Project.getProjectFromJson(projectObj)
+
+                }
+            }
+        })
+    }
 }
--- app/src/main/java/com/neppplus/daily10minutes_difficult_20210418/datas/Project.kt
+++ app/src/main/java/com/neppplus/daily10minutes_difficult_20210418/datas/Project.kt
@@ -1,8 +1,36 @@
 package com.neppplus.daily10minutes_difficult_20210418.datas
 
-class Project (
-        var id : Int,
-        var title : String,
-        var imageUrl : String,
-        var description : String) {
+import org.json.JSONObject
+
+class Project(
+    var id: Int,
+    var title: String,
+    var imageUrl: String,
+    var description: String
+) {
+
+//        보조 생성자를 추가하자. Project() 만으로도 쓸 수 있게 하기위해서!!
+
+    constructor() : this(0, "", "", "")
+
+//        기능을 추가하자
+//        JSONObject 을 넣으면 (input)
+//        Project 로 변환 (return)  => 단순 기능은 companion object 를 이용하는게 편함
+
+    companion object {
+
+        fun getProjectFromJson(jsonObj : JSONObject) : Project {
+
+            val project = Project()
+
+//            jsonObj 에서 정보 추출해서 project 의 하위 항목을 반영하자
+            project.id = jsonObj.getInt("id")
+            project.title = jsonObj.getString("title")
+            project.imageUrl = jsonObj.getString("img_url")
+            project.description = jsonObj.getString("description")
+
+            return project
+
+        }
+    }
 }

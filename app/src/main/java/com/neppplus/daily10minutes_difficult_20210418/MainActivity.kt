81956d3cc4bc9be4ca42ef0c50519eea6c357263
전은형 <ooooo5h91@gmail.com>
2021-04-19 21:36:23 +0900

    로그아웃 버튼 추가해서 누르면 확인창 까지띄움

--- app/src/main/java/com/neppplus/daily10minutes_difficult_20210418/MainActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_difficult_20210418/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.neppplus.daily10minutes_difficult_20210418
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.neppplus.daily10minutes_difficult_20210418.adapters.ProjectAdapter
 import com.neppplus.daily10minutes_difficult_20210418.datas.Project
+import com.neppplus.daily10minutes_difficult_20210418.utils.ContextUtil
 import com.neppplus.daily10minutes_difficult_20210418.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
@@ -21,6 +24,20 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        logoutBtn.setOnClickListener {
+
+            val alert = AlertDialog.Builder(mContext)
+            alert.setTitle("로그아웃")
+            alert.setMessage("정말 로그아웃하시겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
+
+
+            })
+            alert.setNegativeButton("취소", null)
+            alert.show()
+
+        }
 
     }
 

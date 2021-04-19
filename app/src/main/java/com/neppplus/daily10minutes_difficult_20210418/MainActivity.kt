0afc6262597658a460bb2c3c75d2f43bdebeccfc
전은형 <ooooo5h91@gmail.com>
2021-04-19 21:40:42 +0900

    로그아웃시 토큰을 빈값으로 만들어줌

--- app/src/main/java/com/neppplus/daily10minutes_difficult_20210418/MainActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_difficult_20210418/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.neppplus.daily10minutes_difficult_20210418
 
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
@@ -25,19 +26,26 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
-        logoutBtn.setOnClickListener {
+       logoutBtn.setOnClickListener {
 
-            val alert = AlertDialog.Builder(mContext)
-            alert.setTitle("로그아웃")
-            alert.setMessage("정말 로그아웃하시겠습니까?")
-            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
+           val alert = AlertDialog.Builder(mContext)
+           alert.setTitle("로그아웃")
+           alert.setMessage("정말 로그아웃 하시겠습니까?")
+           alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
+
+               ContextUtil.setLoginToken(mContext, "")
+
+               val myIntent = Intent(mContext, LoginActivity::class.java)
+               startActivity(myIntent)
+
+               finish()
+
+           })
+           alert.setNegativeButton("취소", null)
+           alert.show()
+       }
 
 
-            })
-            alert.setNegativeButton("취소", null)
-            alert.show()
-
-        }
 
     }
 
